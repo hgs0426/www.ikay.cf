766d0e4d3916c397cc10711b3f52f0d429cf04b8
hwang <hgs0426@gmail.com>
2019-03-06 00:20:08 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const config = require(path.join(__dirname, 'config.json')).web;
+
+const app = express();
+
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+
+  res.sendFile(path.join(__dirname, 'public','html','index.html'));
+
+});
+
+app.listen(config.port, () => console.log(`Example app listening on port ${config.port}!`))
