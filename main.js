9e387b44201dd74fcc113b995ad4ea12bc6d3db0
hwang <hgs0426@gmail.com>
2019-03-06 23:12:12 +0900

    first commit

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const config = require(path.join(__dirname, 'config.json')).web;
 
 const app = express();
-
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
