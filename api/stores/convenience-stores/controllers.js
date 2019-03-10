9bd5e6e101dcd8faf16687531d38b0e4b61857bb
hwang <hgs0426@gmail.com>
2019-03-10 22:30:43 +0900

    add auth with jwt

--- api/stores/convenience-stores/controllers.js
+++ api/stores/convenience-stores/controllers.js
@@ -2,21 +2,51 @@ const path = require('path');
 const Requester = require(path.join(__dirname, '../', '../', '../', 'libs', 'Requester'));
 const config = require(path.join(__dirname, '../', '../', '../', 'config')).api;
 
-const serveConvenienceStores = async (req, res) => {
-  const requester =  new Requester();
+const getJwt = (body) => {
+  return new Promise ( async (resolve, reject)=> {
+    const requester =  new Requester();
     const options = {
+      hostname: config.hostname,
+      port: config.port,
+      path: '/api/auth/login',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    try {
+      const results = await requester.http(options, body);
+      // console.log(results); 
+      resolve(results);
+    } catch (err) {
+      rejcet(err);
+    }
+  });
+}
+
+const serveConvenienceStores = async (req, res) => {
+
+  const results = await getJwt(config.userInfo);
+  if ( 200 !== results.statusCode ) {
+    res.status(results.statusCode).json(results);
+  }
+  const requester =  new Requester();
+  const options = {
     hostname: config.hostname,
     port: config.port,
     path: '/api/stores/convenience-stores',
-    method: 'GET'
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${results.body.jwt}`
+    }
   };
-  // console.log(options);  
   try {
     const results = await requester.http(options);
-    // console.log(results); 
     res.json(results);
   } catch (err) {
-    throw new Error(err);
+    res.status(500).json(err);
   }
 };
 
