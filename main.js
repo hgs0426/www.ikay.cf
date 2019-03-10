6bc58808ff90310c716cfb7a087ae52690564c80
hwang <hgs0426@gmail.com>
2019-03-10 14:08:40 +0900

    remove cors module

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 
 const config = require(path.join(__dirname, 'config.json')).web;
 
 const app = express();
-app.use(cors({
-  origin: '*',
-  credentials: true
-}));
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-
   res.sendFile(path.join(__dirname, 'public','html','index.html'));
-
 });
 
 app.listen(config.port, () => console.log(`Example app listening on port ${config.port}!`))
+
